4b1dc71fb13d1a1446cdf989c4ffd838329efff1
Oleksii <o.narovskyi@gmail.com>
2023-07-21 16:22:04 +0300

    finish app

---
+++ src/components/Contacts/Contacts.jsx
@@ -0,0 +1,31 @@
+import { Title, List, Button, ListItem } from "./Contacts.styled";
+import PropTypes from 'prop-types';
+
+export default function Contacts({ contacts, onClick }) {
+    return (
+        <>
+            <Title>Contacts</Title>
+            <List>
+                {contacts.map(({ name, id, number }) => (
+                    <ListItem key={id}>
+                        <Button onClick={() => { onClick(id) }}>
+                            Delete
+                        </Button>
+                        {name}: <span>{number}</span>
+                    </ListItem>
+                ))}
+            </List>
+        </>
+    );
+}
+
+Contacts.propTypes = {
+    contacts: PropTypes.arrayOf(
+        PropTypes.exact({
+            name: PropTypes.string.isRequired,
+            number: PropTypes.string.isRequired,
+            id: PropTypes.string.isRequired
+        })
+    ).isRequired,
+    onClick: PropTypes.func.isRequired
+}
